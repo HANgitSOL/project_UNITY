15b364ada81026632f83c3b7ea34503390f35c64
Hansol Jenya Lee <120706982+HANgitSOL@users.noreply.github.com>
2023-01-13 19:27:13 +0900

    Add files via upload

---
+++ week_2/ball/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private DateTime TimeRestrict = System.DateTime.Now.TimeOfDay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DateTime thisTime = System.DateTime.Now.TimeOfDay;
+            if (thisTime - TimeRestrict > 5)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+            DateTime TimeRestrict = System.DateTime.Now.TimeOfDay;
+        }
+    }
+}
